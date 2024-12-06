858bc4a10b2d275131999b6532c47f98638a9240
Antonio Vass <152070597+saint-simone@users.noreply.github.com>
2024-12-06 18:26:10 +0200

    modified:   .gitignore 	new file:   components.json 	new file:   drizzle.config.ts 	modified:   next.config.ts 	modified:   package-lock.json 	modified:   package.json 	new file:   src/app/(auth)/layout.tsx 	new file:   src/app/(auth)/sign-in/[[...sign-in]]/page.tsx 	new file:   src/app/(auth)/sign-up/[[...sign-up]]/page.tsx 	new file:   src/app/(private)/events/[eventId]/edit/page.tsx 	new file:   src/app/(private)/events/new/page.tsx 	new file:   src/app/(private)/events/page.tsx 	new file:   src/app/(private)/layout.tsx 	new file:   src/app/(private)/schedule/page.tsx 	new file:   src/app/(public)/book/[clerkUserId]/[eventId]/loading.tsx 	new file:   src/app/(public)/book/[clerkUserId]/[eventId]/page.tsx 	new file:   src/app/(public)/book/[clerkUserId]/[eventId]/success/page.tsx 	new file:   src/app/(public)/book/[clerkUserId]/page.tsx 	new file:   src/app/(public)/layout.tsx 	modified:   src/app/globals.css 	modified:   src/app/layout.tsx 	modified:   src/app/page.tsx 	new file:   src/components/CopyEventButton.tsx 	new file:   src/components/NavLink.tsx 	new file:   src/components/forms/EventForm.tsx 	new file:   src/components/forms/MeetingForm.tsx 	new file:   src/components/forms/ScheduleForm.tsx 	new file:   src/components/ui/alert-dialog.tsx 	new file:   src/components/ui/button.tsx 	new file:   src/components/ui/calendar.tsx 	new file:   src/components/ui/card.tsx 	new file:   src/components/ui/form.tsx 	new file:   src/components/ui/input.tsx 	new file:   src/components/ui/label.tsx 	new file:   src/components/ui/popover.tsx 	new file:   src/components/ui/select.tsx 	new file:   src/components/ui/switch.tsx 	new file:   src/components/ui/textarea.tsx 	new file:   src/data/constants.ts 	new file:   src/drizzle/db.ts 	new file:   src/drizzle/migrations/0000_first_whistler.sql 	new file:   src/drizzle/migrations/meta/0000_snapshot.json 	new file:   src/drizzle/migrations/meta/_journal.json 	new file:   src/drizzle/schema.ts 	new file:   src/lib/formatters.ts 	new file:   src/lib/getValidTimesFromSchedule.ts 	new file:   src/lib/utils.ts 	new file:   src/middleware.ts 	new file:   src/schema/events.ts 	new file:   src/schema/meetings.ts 	new file:   src/schema/schedule.ts 	new file:   src/server/actions/events.ts 	new file:   src/server/actions/meetings.ts 	new file:   src/server/actions/schedule.ts 	new file:   src/server/googleCalendar.ts 	modified:   tailwind.config.ts

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,15 @@
+import { auth } from "@clerk/nextjs/server"
+import { redirect } from "next/navigation"
+import { ReactNode } from "react"
+
+export default function AuthLayout({ children }: { children: ReactNode }) {
+  auth().then(({ userId }) => {
+    if (userId != null) redirect("/")
+  })
+
+  return (
+    <div className="min-h-screen flex flex-col justify-center items-center">
+      {children}
+    </div>
+  )
+}
